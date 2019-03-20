6e0984b3a4bc9cbf98875971809bdf67470a9665
Arjasa Virya <arjasa1103@gmail.com>
2019-03-20 19:42:37 +0700

    remove unnecessary component

--- views/common/components/banner-item.directive.ts
+++
@@ -1,9 +0,0 @@
-import {Directive, TemplateRef} from '@angular/core';
-
-@Directive({selector: '[carouselItem]'})
-export class BannerItemDirective {
-
-    constructor(public tpl: TemplateRef<any>){
-
-    };
-}
