9a1950a745380cd9f6a4c2310eac486add34f742
Arjasa Virya <arjasa1103@gmail.com>
2019-03-23 15:58:11 +0700

    Added item page banner

---
+++ views/items/items.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from '@angular/core';
+import {ITEMS_ROUTING} from '@views/items/items.route';
+import {CommonViewModule} from '@views/common/common.module';
+import {CommonModule} from '@angular/common';
+import {NguCarouselModule} from '@ngu/carousel';
+import {ItemsPage} from '@views/items/pages/items.page';
+import {FeaturedBannerComponent} from '@views/items/components/featured-banner/featured-banner.component';
+import {ItemListComponent} from '@views/items/components/item-list/item-list.component';
+
+const COMPONENTS = [
+    FeaturedBannerComponent,
+    ItemListComponent
+];
+
+const CONTAINERS = [
+
+];
+
+const PAGES = [
+    ItemsPage
+];
+
+@NgModule({
+    declarations: [
+        ...COMPONENTS,
+        ...CONTAINERS,
+        ...PAGES,
+    ],
+    imports: [
+        ITEMS_ROUTING,
+        CommonModule,
+        CommonViewModule,
+        NguCarouselModule
+    ],
+})
+export class ItemsViewModule {}
