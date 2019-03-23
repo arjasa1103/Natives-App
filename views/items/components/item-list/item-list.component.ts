9812a99114f29dfda26b6a149df6a402f02506a8
Arjasa Virya <arjasa1103@gmail.com>
2019-03-23 20:37:31 +0700

    Added item page banner

--- views/items/components/item-list/item-list.component.ts
+++ views/items/components/item-list/item-list.component.ts
@@ -1,9 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'item-list',
     templateUrl: 'item-list.component.html',
 })
 export class ItemListComponent {
+    @Input() itemList : any;
+    public dummylist = [];
 
+    constructor(){
+        const item = {   itemImg:"https://via.placeholder.com/300.png",
+            brandName:"Brand Name",
+            itemName:"Item Name",
+            itemPrice:"IDR 123456"};
+        for (var i = 0; i < 60; i++) {
+            this.dummylist.push(item);
+        }
+    }
 }
