17093029401326d095206ccbc1d765e6eff34faa
Arjasa Virya <arjasa1103@gmail.com>
2019-03-24 16:11:23 +0700

    Finished item list styling

--- views/common/common.module.ts
+++ views/common/common.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {ItemCardComponent} from '@views/common/components/item.card';
+import {ItemCardComponent} from '@views/common/components/item-card/item.card';
 import {FooterComponent} from '@views/common/components/footer.component';
 import {HOME_ROUTING} from '@views/home/home.route';
 import {CommonModule} from '@angular/common';
