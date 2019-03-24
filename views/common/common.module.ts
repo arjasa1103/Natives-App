c516a5cd1693c5122da88038f6ab71d3ef592023
Arjasa Virya <arjasa1103@gmail.com>
2019-03-24 16:11:23 +0700

    Finished item list styling

--- views/common/common.module.ts
+++ views/common/common.module.ts
@@ -1,13 +1,12 @@
 import {NgModule} from '@angular/core';
 import {ItemCardComponent} from '@views/common/components/item-card/item.card';
-import {FooterComponent} from '@views/common/components/footer/footer.component';
+import {FooterComponent} from '@views/common/components/footer.component';
 import {HOME_ROUTING} from '@views/home/home.route';
 import {CommonModule} from '@angular/common';
 import {MatCardModule, MatDialogModule} from '@angular/material';
 import {NavbarComponent} from '@views/common/components/navbar/navbar.component';
 import {DialogComponent} from '@views/common/components/dialog/dialog.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormViewModule} from '@views/form/form.module';
 
 const COMPONENTS = [
