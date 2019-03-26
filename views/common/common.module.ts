0e014012290fe55b24df1cdfef3ebc70a9d9a44a
Arjasa Virya <arjasa1103@gmail.com>
2019-03-26 23:02:30 +0700

    fix common module

--- views/common/common.module.ts
+++ views/common/common.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {ItemCardComponent} from '@views/common/components/item-card/item.card';
-import {FooterComponent} from '@views/common/components/footer.component';
+import {FooterComponent} from '@views/common/components/footer/footer.component';
 import {CommonModule} from '@angular/common';
 import {MatCardModule, MatDialogModule} from '@angular/material';
-import {NavbarComponent} from '@views/common/components/navbar.component';
-import {DialogComponent} from '@views/common/components/dialog.component';
+import {NavbarComponent} from '@views/common/components/navbar/navbar.component';
+import {DialogComponent} from '@views/common/components/dialog/dialog.component';
 import {FormViewModule} from '@views/form/form.module';
 
 const COMPONENTS = [
